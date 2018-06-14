2d487f81942395956b10b0999783645264e6ef56
TAUSIF AHMED <tausif369@gmail.com>
2018-06-14 14:37:06 +0530

    GET /:id Route Added

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./Models/todo');
 var {User} = require('./Models/user');
@@ -27,6 +27,21 @@ app.get('/todos', (req, res) => {
   },(e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(404).send();
+    }
+    res.send({todo})
+  }).catch( (err) => res.status(400).send());
+
 });
 
 app.listen(port, () => {
